6b8f8b1c601c770bdd9b3c7bc96a6c0ad90705e0
John Woodell <woodie@netpress.com>
2026-07-01 10:42:01 -0700

    Bump zouk to 1.7.0

--- Casks/zouk.rb
+++ Casks/zouk.rb
@@ -7,8 +7,8 @@ cask "zouk" do
   # `version` used for both (the previous approach) builds a tag that
   # doesn't exist (v1.5 instead of v1.5.0) and 404s on download. Bump
   # both parts together whenever woodie/zouk cuts a new tagged release.
-  version "1.6.0,1.6"
-  sha256 "45cb09f3a78d269163938aeffb4f29da434217f137abf7d2a907c818a7c642b2"
+  version "1.7.0,1.7"
+  sha256 "ef03219c6aea63c706e7266833583adc4cccd7188dbb6b523e7200a59a6d40c3"
 
   url "https://github.com/woodie/zouk/releases/download/v#{version.before_comma}/zouk-#{version.after_comma}.zip"
   name "Zouk"
